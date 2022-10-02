296fc38660f480f30ea8b70778b196a8da9a86bf
Jakub Domagała <falonix01@gmail.com>
2022-10-02 18:19:12 +0200

    Added mobile form styles

--- public/scripts/hamburger.js
+++ public/scripts/hamburger.js
@@ -1,8 +1,22 @@
 const hamburgerMenuBtnElement = document.getElementById("hamburger-icons");
 const hamburgerMenuElement = document.getElementById("hamburger-menu");
 
+const registerElement = document.getElementById("register-section");
+const loginElement = document.getElementById("login-section");
+
 function toggleMenu() {
     hamburgerMenuElement.classList.toggle("active");
 }
 
+function adjustExtras() {
+    if(!registerElement.classList.contains("is-invisible")) {
+        registerElement.classList.toggle("is-invisible")
+    }
+
+    if(!loginElement.classList.contains("is-invisible")) {
+        loginElement.classList.toggle("is-invisible")
+    }
+}
+
 hamburgerMenuBtnElement.addEventListener("click", toggleMenu);
+hamburgerMenuBtnElement.addEventListener("click", adjustExtras)
